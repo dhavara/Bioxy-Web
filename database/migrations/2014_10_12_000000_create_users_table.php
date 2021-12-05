0f72253569c1c57fa33bb86e5df980e4479b264a
Enrico <enricowakiman9@gmail.com>
2021-12-05 21:27:04 +0700

    laravel passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bio11_users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('school');
+            $table->string('city');
+            $table->string('birthdate');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'Admin',
+            'name' => 'KELD Administrator',
+            'email' => 'admin@keld2021.com',
+            'password' => Hash::make('KELD_Admin'),
+            'school' => 'Universitas Ciputra',
+            'city' => 'Surabaya',
+            'created_at' => \Carbon\Carbon::now(),
+            'email_verified_at' => \Carbon\Carbon::now()
+        ]);
+        DB::table('users')->insert([
+            'username' => 'User',
+            'name' => 'Awanama',
+            'email' => 'user@keld2021.com',
+            'password' => Hash::make('12345678'),
+            'school' => 'Lorem Ipsum High School',
+            'city' => 'Dolor City',
+            'created_at' => \Carbon\Carbon::now(),
+            'email_verified_at' => \Carbon\Carbon::now()
+        ]);
+    }
+}
