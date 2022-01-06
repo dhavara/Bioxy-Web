58a02e6e3e36c46de0bee6f0b5a6ba9ad0e84ea9
Kevinwlliam <kwilliam02@student.ciputra.ac.id>
2022-01-06 19:31:44 +0800

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,72 +2,155 @@
 
 @section('content')
 
-    <div id="section1" class="container bg-secondary text-white text-center" style="padding:100px;">
-        <h1>Welcome to Bioxy Website</h1>
-        <br>
-        <p class= "alert alert-dark">This place where you can knows everything about Biology and improve your with aesthetic standard visual design!</p>
-    </div>
+<div class="ex-page-content bootstrap snippets bootdeys">
+  <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="message-box">
+              <h1>Welcome to Bioxy Website</h1>
+              <p class="bg-text">Desc</p>
+          </div>
+        </div>
+          <div class="col-sm-6">
+              <svg class="svg-box" width="380px" height="500px" viewBox="0 0 837 1045">
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none">
+                      <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Bioxy-1" stroke="#3bafda" stroke-width="6" ></path>
+                      <path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Bioxy-2" stroke="#7266ba" stroke-width="6" ></path>
+                      <path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Bioxy-3" stroke="#f76397" stroke-width="6" ></path>
+                      <path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Bioxy-4" stroke="#00b19d" stroke-width="6" ></path>
+                      <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Bioxy-5" stroke="#ffaa00" stroke-width="6" ></path>
+                  </g>
+              </svg>
+          </div>
+      </div>
+  </div>
+</div>
 
-  <div class="container mt-5">
+<br><br><br><br><br><br>
+
+  <div id="section1" class="container bg-secondary text-white text-center" style="padding:100px;">
+      <div class="row justify-content-center text-center">
+        <div class="col-lg-12">
+            <h2 class="font-300">Cara Kerja Bioxy?</h2>
+            <p class="font-300 bg-text">Disinilah tempat dimana kita dapat belajar bagaimana menggunakan bioxy dengan bijak.</p>
+        </div>
+    </div>
+    <div class="timeline-page mt-5">
+        <div class="timeline-item">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="duration date-label-left">
+                        <img src="https://themesdesign.in/landsay/images/how-it-1.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="works works-description-right">
+                        <h4>buatlah sebuah akun dengan register</h4>
+                        <p class="timeline-subtitle">Buatlah akun dengan mengisi biodata setelah menekan tombol register agar dapat memulai Bioxy dan mendapatkan manfaat dari aplikasi kami. Tenang aja semua data anda akan aman dijaga oleh perusahaan kami :)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="timeline-item">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="works works-description-left">
+                        <h4>bermain dengan menekan tombol play</h4>
+                        <p class="timeline-subtitle">Setelah anda membuat akun, ditampilan bioxy anda akan muncul tombol play, dimana anda akan memulai bioxy yang sebenarnya, sebelum menekan tombol play disarankan untuk mempersiapkan diri dengan belajar yang giat :)</p>
+                        <p class="timeline-subtitle">Semangat teman-teman!</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="duration duration-right ">
+                    <img src="https://themesdesign.in/landsay/images/how-it-2.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="timeline-item">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="duration date-label-left">
+                      <img src="https://themesdesign.in/landsay/images/how-it-3.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="works works-description-right">
+                        <h4>belajar dari kekurangan</h4>
+                        <p class="timeline-subtitle">Setelah selesai bermain bioxy pasti banyak pelajaran yang kita dapat, disinilah waktunya untuk belajar dari kesalahan-kesalahan kita saat bermain bioxy dan mempersiapkan diri untuk bioxy selanjutnya!</p>
+                        <p class="timeline-subtitle">Tetap semangat yaa teman-teman :)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <br><br><br>
+
+<div class="container bootstrap snippets bootdey">
     <div class="row">
-      <div class="col-sm-4">
-        <h2 class="mt-4">Bioxy Access</h2>
-        <p>You can access our website with this link</p>
-        <ul class="nav nav-pills flex-column">
-          <li class="nav-item btn btn-secondary">
-            <a class="nav-link" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">Instagram</a>
-          </li>
-          <br>
-          <li class="nav-item btn btn-secondary">
-            <a class="nav-link" href="https://www.youtube.com/watch?v=aCgP8BFjrw4&ab_channel=vector">Facebook</a>
-          </li>
-          <br>
-          <li class="nav-item btn btn-secondary">
-            <a class="nav-link" href="https://www.youtube.com/watch?v=cKw3rtDnPK4&ab_channel=LordKazuma">YouTube</a>
-          </li>
-        </ul>
-        <hr class="d-sm-none">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="panel panel-dark panel-colorful">
+                <div class="panel-body text-center">
+                	<p class="text-uppercase mar-btm text-sm">Desc</p>
+                  <li class="nav-item btn btn-secondary">
+                    <a class="nav-link" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">Instagram</a>
+                  </li>
+                	<hr>
+                	<small><span class="text-semibold"></span> Desc</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+        	<div class="panel panel-danger panel-colorful">
+        		<div class="panel-body text-center">
+        			<p class="text-uppercase mar-btm text-sm">Desc</p>
+              <li class="nav-item btn btn-secondary">
+                <a class="nav-link" href="https://www.youtube.com/watch?v=aCgP8BFjrw4&ab_channel=vector">Facebook</a>
+              </li>
+        			<hr>
+        			<small><span class="text-semibold"><i class="fa fa-unlock-alt fa-fw"></i></span> Desc</small>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+        	<div class="panel panel-primary panel-colorful">
+        		<div class="panel-body text-center">
+        			<p class="text-uppercase mar-btm text-sm">Desc</p>
+              <li class="nav-item btn btn-secondary">
+                <a class="nav-link" href="https://www.youtube.com/watch?v=cKw3rtDnPK4&ab_channel=LordKazuma">YouTube</a>
+              </li>
+        			<hr>
+        			<small><span class="text-semibold"><i class="fa fa-shopping-cart fa-fw"></i></span> Desc</small>
+        		</div>
+        	</div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+        	<div class="panel panel-info panel-colorful">
+        		<div class="panel-body text-center">
+        			<p class="text-uppercase mar-btm text-sm">Earning</p>
+              <h3>Bioxy Short Story</h3>
+              <button class="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
+                Open Story
+              </button>
+        			<hr>
+        			<small><span class="text-semibold"><i class="fa fa-dollar fa-fw"></i></span> Short story about how we can build Bioxy</small>
+        		</div>
+        	</div>
+        </div>        
+	</div>
+</div>
 
-        <br><br><br><br>
-
-        <h2>About us</h2>
-        <h5>Photo of our Students:</h5>
-        <img src="/img/female.jpg" width="300px" height="350px">
-        <p>Picture of one of our students in Univesity of California Los Angeles.</p>
-      </div>
-
-      <div class="col-sm-8">
-        <h2>Achievement</h2>
-        <h5>Description</h5>
-        <img src="/img/scientist.jpg" width="750px" height="300px">
-        <p>Description</p>
-        <p>Description. </p>
-  
-        <h2 class="mt-5">Achievement</h2>
-        <h5>Description</h5>
-        <img src="/img/gathering.jpg" width="750px" height="300px">
-        <p>Description</p>
-        <p>Description. </p>
-      </div>
-    </div>
+<div class="offcanvas offcanvas-start" id="demo">
+  <div class="offcanvas-header">
+    <h2 class="offcanvas-title">Bioxy's Story</h2>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
   </div>
-
-  <div class="offcanvas offcanvas-start" id="demo">
-    <div class="offcanvas-header">
-      <h2 class="offcanvas-title">Bioxy's Story</h2>
-      <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-    </div>
-    <div class="offcanvas-body">
-      <p class="colorp">Bioxy.</p>
-    </div>
+  <div class="offcanvas-body">
+    <p class="colorp">Bioxy</p>
   </div>
-
-  <div class="mt-5">
-    <h3>Bioxy Short Story</h3>
-    <p>Short story about how we can build some eCommerce like Bioxy</p>
-    <button class="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
-      Open Story
-    </button>
-  </div>
+</div>
 
 @endsection
