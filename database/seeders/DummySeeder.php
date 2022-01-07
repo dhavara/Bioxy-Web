98df9436e21aaf062d46c75780c972c0ae1f0b16
dhavara <dhava.rachmana.aziz@gmail.com>
2022-01-07 09:44:03 +0700

    nambah data dummy

--- database/seeders/DummySeeder.php
+++ database/seeders/DummySeeder.php
@@ -25,15 +25,14 @@ class DummySeeder extends Seeder
             'school' => 'Midtown High School',
             'city' => 'New York City',
             'birthdate' => strtotime('10 August 2001'),
-            'created_at' => \Carbon\Carbon::now(),
-            'email_verified_at' => \Carbon\Carbon::now()
+            'created_at' => date('Y-d-m H:i:s', strtotime('7 January 2021 07:10:25'))
         ]);
             // Detail
         DB::table('bio11_users_details')->insert([
             'user_id' => '7', // User
             'user_title' => '1',
             'point' => '600',
-            'created_at' => \Carbon\Carbon::now(),
+            'created_at' => date('Y-d-m H:i:s', strtotime('7 January 2021 07:10:25')),
         ]);
             // Titles
         DB::table('bio11_users_titles')->insert([
@@ -62,9 +61,59 @@ class DummySeeder extends Seeder
             'accuracy' => '65',
             'total_correct' => '15',
             'total_question' => '23',
-            'created_at' => date('Y-d-m H:i:s', strtotime('7 January 2021 07:12:25')),
+            'created_at' => date('Y-d-m H:i:s', strtotime('7 January 2021 07:32:23')),
         ]);
         // NotSpiderman
 
+        // Lunar
+            // User
+        DB::table('bio11_users')->insert([
+            'username' => 'Mulan',
+            'name' => 'Liu Yanfei',
+            'email' => 'liuyanfei@gmail.com',
+            'password' => Hash::make('12345678'),
+            'school' => 'Beijing High School',
+            'city' => 'Beijing',
+            'birthdate' => strtotime('20 September 1999'),
+            'created_at' => date('Y-d-m H:i:s', strtotime('8 January 2021 10:09:47')),
+            'email_verified_at' => date('Y-d-m H:i:s', strtotime('8 January 2021 10:10:20'))
+        ]);
+            // Detail
+        DB::table('bio11_users_details')->insert([
+            'user_id' => '8', // User
+            'user_title' => '1',
+            'point' => '420',
+            'created_at' => date('Y-d-m H:i:s', strtotime('8 January 2021 10:09:47')),
+        ]);
+            // Titles
+        DB::table('bio11_users_titles')->insert([
+            'user_id' => '8',
+            'title_id' => '1'
+        ]);
+            // Roles
+        DB::table('bio11_users_roles')->insert([
+            'user_id' => '8',
+            'role_id' => '1',
+        ]);
+            // Histories
+        DB::table('bio11_users_histories')->insert([
+            'user_id' => '8', // User
+            'difficulty' => '3',
+            'point' => '90', //jumlah benar dikali 10
+            'accuracy' => '69', //jumlah benar dibagi jumlah soal
+            'total_correct' => '9',
+            'total_question' => '13',
+            'created_at' => date('Y-d-m H:i:s', strtotime('9 January 2021 14:35:55')),
+        ]);
+        DB::table('bio11_users_histories')->insert([
+            'user_id' => '8', // User
+            'difficulty' => '1',
+            'point' => '150',
+            'accuracy' => '65',
+            'total_correct' => '15',
+            'total_question' => '23',
+            'created_at' => date('Y-d-m H:i:s', strtotime('10 January 2021 08:43:21')),
+        ]);
+        // Mulan
     }
 }
