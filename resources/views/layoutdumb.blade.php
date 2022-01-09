3aba22f865fe55ec093c25bdc560bc3da2b6fcd2
Kevinwlliam <kwilliam02@student.ciputra.ac.id>
2022-01-09 17:31:46 +0800

    dumb

---
+++ resources/views/layoutdumb.blade.php
@@ -0,0 +1,24 @@
+<div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="panel panel-danger panel-colorful">
+        <div class="panel-body text-center">
+            <p class="text-uppercase mar-btm text-sm">Desc</p>
+      <li class="nav-item btn btn-secondary">
+        <a class="nav-link" href="https://www.youtube.com/watch?v=aCgP8BFjrw4&ab_channel=vector">Facebook</a>
+      </li>
+            <hr>
+            <small><span class="text-semibold"><i class="fa fa-unlock-alt fa-fw"></i></span> Desc</small>
+        </div>
+    </div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="panel panel-primary panel-colorful">
+        <div class="panel-body text-center">
+            <p class="text-uppercase mar-btm text-sm">Desc</p>
+      <li class="nav-item btn btn-secondary">
+        <a class="nav-link" href="https://www.youtube.com/watch?v=cKw3rtDnPK4&ab_channel=LordKazuma">YouTube</a>
+      </li>
+            <hr>
+            <small><span class="text-semibold"><i class="fa fa-shopping-cart fa-fw"></i></span> Desc</small>
+        </div>
+    </div>
+</div>
