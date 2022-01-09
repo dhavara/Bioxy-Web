0a85a417855320f6eb8551108d2d17db53c454e1
Enrico <enricowakiman9@gmail.com>
2022-01-09 23:53:17 +0700

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -227,7 +227,7 @@ h2, .h2 {
     <div class="container">
         <nav class="navbar navbar-expand-md navbar-primary bg-secondary shadow-sm fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Bioxy') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
