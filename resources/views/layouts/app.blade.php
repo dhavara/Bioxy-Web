8d621260062f5bd447bd3b77164ea9bbeb0e31fa
Enrico <enricowakiman9@gmail.com>
2022-01-10 12:48:32 +0700

    again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     
-    <link href="{{ <?php echo auto_version('/css/base.css'); ?> }}" rel="stylesheet" type="text/css">
+    <link href="http://bioxy.keldteam.xyz/css/stylesheet.css" rel="stylesheet" type="text/css">
 
     <!-- Slick -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"/>
